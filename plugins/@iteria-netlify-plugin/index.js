a2573b31f05f843a7e36ac51ca95692ed624638a
mecirmartin <mecir.martin@gmail.com>
2021-06-23 16:56:22 +0200

    ommit v flag

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -5,7 +5,7 @@ module.exports = {
   onPreBuild: async ({ utils: { run } }) => {
     // await run.command('ls -a');
     await run.command(
-      'tar --exclude ./node_modules --exclude ./node_modules  --exclude node_modules -zcvf ./public/Sources.tar.gz ./'
+      'tar --exclude ./node_modules --exclude ./node_modules  --exclude node_modules -zcf ./public/Sources.tar.gz ./'
     );
     getNewPackageJson();
     generateIndex();
