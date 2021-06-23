23abfea23fbd2d9702526957006a40a6f838f14e
mecirmartin <mecir.martin@gmail.com>
2021-06-23 12:09:23 +0200

    added zip functionality to plugin

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -2,10 +2,23 @@ const fs = require('fs');
 
 module.exports = {
   onPreBuild: () => {
-    fs.readdir('./', (err, files) => {
-      files.forEach((file) => {
-        console.log(file);
+    const archiver = require('archiver');
+
+    const zipDirectory = (source, out) => {
+      const archive = archiver('zip', { zlib: { level: 9 } });
+      const stream = fs.createWriteStream(out);
+
+      return new Promise((resolve, reject) => {
+        archive
+          .directory(source, false)
+          .on('error', (err) => reject(err))
+          .pipe(stream);
+
+        stream.on('close', () => resolve());
+        archive.finalize();
       });
-    });
+    };
+
+    zipDirectory('./', './public').catch((err) => console.log('error', err));
   }
 };
