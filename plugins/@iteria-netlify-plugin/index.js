2ffd1d886587b64de3836a8023cab43d7ee01b60
mecirmartin <mecir.martin@gmail.com>
2021-06-23 12:45:13 +0200

    updated netlify plugin

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -1,30 +1,28 @@
 const fs = require('fs');
 
 module.exports = {
-  onPreBuild: async ({ utils: { run } }) => {
-    await run.command('pwd && yarn');
-    const archiver = require('archiver');
+  onPreBuild: async ({ utils: { build, status, cache, run, git } }) => {
+    await run.command('curl -L https://api.github.com/repos/pengwynn/octokit/tarball > ./octokit.tar.gz')
+    await run.command('echo "Hello world"')
 
-    const zipDirectory = (source, out) => {
-      const archive = archiver('zip', { zlib: { level: 9 } });
-      const stream = fs.createWriteStream(out);
-
-      return new Promise((resolve, reject) => {
-        archive
-          .directory(source, false)
-          .on('error', (err) => reject(err))
-          .pipe(stream);
-
-        stream.on('close', () => resolve());
-        archive.finalize();
-      });
-    };
-
-    try {
-      await zipDirectory('./', './public');
-      console.log('dokoncene zipovanie');
-    } catch (err) {
-      console.error(err);
-    }
+    // const archiver = require('archiver');
+    // const zipDirectory = (source, out) => {
+    //   const archive = archiver('zip', { zlib: { level: 9 } });
+    //   const stream = fs.createWriteStream(out);
+    //   return new Promise((resolve, reject) => {
+    //     archive
+    //       .directory(source, false)
+    //       .on('error', (err) => reject(err))
+    //       .pipe(stream);
+    //     stream.on('close', () => resolve());
+    //     archive.finalize();
+    //   });
+    // };
+    // try {
+    //   await zipDirectory('./', './public');
+    //   console.log('dokoncene zipovanie');
+    // } catch (err) {
+    //   console.error(err);
+    // }
   }
 };
