92977d9a1db3c818b4d8423e0e8c84b5a722883b
mecirmartin <mecir.martin@gmail.com>
2021-06-23 17:11:47 +0200

    updated command

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -4,7 +4,7 @@ const { generateIndex } = require('./generateIndex');
 module.exports = {
   onPreBuild: async ({ utils: { run } }) => {
     await run.command(
-      'tar --warning=no-file-changed --exclude node_modules -zcf ./public/Sources.tar.gz ./'
+      'mkdir ./temp_archive && tar --exclude temp_archive --exclude node_modules -zcf ./temp_archive/Sources.tar.gz ./ && mv ./temp_archive/Sources.tar.gz ./public && rm -rf ./temp_archive'
     );
     getNewPackageJson();
     generateIndex();
