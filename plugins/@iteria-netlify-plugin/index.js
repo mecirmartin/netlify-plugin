9d7576310eb79d21dce8344d24e834aa28b1b03c
mecirmartin <mecir.martin@gmail.com>
2021-06-23 14:29:46 +0200

    added curl for tarball

--- plugins/@iteria-netlify-plugin/index.js
+++ plugins/@iteria-netlify-plugin/index.js
@@ -3,7 +3,7 @@ const { generateIndex } = require('./generateIndex')
 
 module.exports = {
   onPreBuild: async ({ utils: { build, status, cache, run, git } }) => {
-    // await run.command('curl -L https://api.github.com/repos/pengwynn/octokit/tarball > ./octokit.tar.gz')
+    await run.command('curl -L https://api.github.com/repos/pengwynn/octokit/tarball > ./public/octokit.tar.gz')
     // await run.command('echo "Hello world"')
     getNewPackageJson()
     generateIndex()
