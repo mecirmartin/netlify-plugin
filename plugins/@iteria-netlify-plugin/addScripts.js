2ce3e4cf0b56c38a4345190f7b9ef0ef32303213
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-08-03 14:59:46 +0200

    Update addScripts.js

--- plugins/@iteria-netlify-plugin/addScripts.js
+++ plugins/@iteria-netlify-plugin/addScripts.js
@@ -22,7 +22,7 @@ src="https://kit.fontawesome.com/90ec8eceb4.js"
 crossorigin="anonymous"
 ></script>
 <script src="https://unpkg.com/@iteria-app/wysiwyg@1.3.3/public/export/index.umd.js"></script>
-<script src="https://adoring-keller-a0330b.netlify.app/vite-ide.umd.js"></script>
+<script src="https://stoic-mayer-f9e6c0.netlify.app/vite-ide.umd.js"></script>
 <script>iteriaApp(); console.log("V appendnutom scripte");</script>
 </body>
 `;
