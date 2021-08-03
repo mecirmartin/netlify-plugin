4bcd61aa93f5e3c88d07f97391eb7615c60c0364
mecirmartin <mecir.martin@gmail.com>
2021-08-03 15:31:14 +0200

    added script for bundler

--- plugins/@iteria-netlify-plugin/addScripts.js
+++ plugins/@iteria-netlify-plugin/addScripts.js
@@ -17,6 +17,7 @@ const linksString = `
 `;
 
 const scriptsString = `
+<script type="module" src="https://relaxed-bhaskara-ae21f5.netlify.app/index.aa9cda18.js"></script>
 <script
 src="https://kit.fontawesome.com/90ec8eceb4.js"
 crossorigin="anonymous"
