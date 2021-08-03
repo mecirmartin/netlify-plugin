ce2c970138b576d24a6515094884aae495040047
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-08-03 14:12:48 +0200

    removed reference to generated file

--- src/views/customer/CustomerListView/index.tsx
+++ src/views/customer/CustomerListView/index.tsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
-import {
-  Box,
-  Container,
-  makeStyles,
-  Card
-} from '@material-ui/core';
+import { Box, Container, makeStyles, Card } from '@material-ui/core';
 import Page from 'src/components/Page';
-import CustomerTable from './CustomerTable';
 import Toolbar from './Toolbar';
-import { useSearchCustomersQuery } from 'src/generated/graphql'
 
 const useStyles = makeStyles((theme: any) => ({
   root: {
@@ -21,28 +14,18 @@ const useStyles = makeStyles((theme: any) => ({
 
 const CustomerListView = () => {
   const classes = useStyles();
-  const [searchCustomer, setSearchCustomer] = useState("");
-  const search = "%" + searchCustomer + "%";
-  const [result] = useSearchCustomersQuery({
-    variables: { search }
-  })
-  const { data, error, fetching } = result;
-
-  if (error) return <p>Oh no... {error.message}</p>;
+  const [searchCustomer, setSearchCustomer] = useState('');
+  const search = '%' + searchCustomer + '%';
 
   return (
     <Page className={classes.root} title="Customers">
       <Container maxWidth={false}>
         <Toolbar
           searchCustomer={searchCustomer}
-          searchCustomerChange={(event) => setSearchCustomer(event.target.value)}
+          searchCustomerChange={event => setSearchCustomer(event.target.value)}
         />
         <Box mt={3}>
-          <Card>
-            <CustomerTable
-              customers={fetching ? null : data.customers}
-            />
-          </Card>
+          <Card></Card>
         </Box>
       </Container>
     </Page>
