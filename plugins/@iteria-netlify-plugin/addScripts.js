a1422232b9072ae00e6de281385a3db853232211
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-08-04 10:34:21 +0200

    Update addScripts.js

--- plugins/@iteria-netlify-plugin/addScripts.js
+++ plugins/@iteria-netlify-plugin/addScripts.js
@@ -4,11 +4,11 @@ const linksString = `
 <head>
 <link
   rel="stylesheet"
-  href="https://unpkg.com/@iteria-app/wysiwyg@1.3.3/public/export/index.css"
+  href="https://unpkg.com/@iteria-app/wysiwyg@1.3.4/public/export/index.css"
 />
 <link
   rel="stylesheet"
-  href="https://unpkg.com/@iteria-app/wysiwyg@1.3.3/public/global.css"
+  href="https://unpkg.com/@iteria-app/wysiwyg@1.3.4/public/global.css"
 />
 <link
   href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -27,7 +27,7 @@ const scriptsString = `
 src="https://kit.fontawesome.com/90ec8eceb4.js"
 crossorigin="anonymous"
 ></script>
-<script src="https://unpkg.com/@iteria-app/wysiwyg@1.3.3/public/export/index.umd.js"></script>
+<script src="https://unpkg.com/@iteria-app/wysiwyg@1.3.4/public/export/index.umd.js"></script>
 <script src="https://wonderful-mclean-d8b4cd.netlify.app/vite-ide.umd.js"></script>
 <script>iteriaApp(); console.log("V appendnutom scripte");</script>
 </body>
